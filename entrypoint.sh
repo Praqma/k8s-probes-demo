232f02b97490d0c3c536bc9faefd613c6dc29fa4
Kamran Azeem <kaz@praqma.net>
2020-09-17 12:10:01 +0200

    Removed unnecessary code from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,11 @@
 TIMESTAMP=$(date +%d-%m-%Y_%T)
 DOCUMENT_ROOT=/usr/share/nginx/html
 
-echo "readinesscheck" > ${DOCUMENT_ROOT}/readinesscheck.txt
-echo "livenesscheck" > ${DOCUMENT_ROOT}/livenesscheck.txt
-
-
-echo "Container started at: ${TIMESTAMP}"
+echo "${TIMESTAMP} - Container started" 
 
 # Check if START_DELAY is defined/set or not:
 if [ "${START_DELAY}" != "" ]; then
-  echo "${TIMESTAMP} - START_DELAY is set - Simulating a slow-start container by sleeping for ${START_DELAY} seconds ..."
+  echo "${TIMESTAMP} - START_DELAY is set - Simulating a slow-starting container by sleeping for ${START_DELAY} seconds ..."
   sleep ${START_DELAY}
 else
   echo "${TIMESTAMP} - START_DELAY was not set, or was set to zero - not sleeping ..."
@@ -18,9 +14,9 @@ fi
 
 # Create index.html with a (new) time-stamp-ed message/heading:
 TIMESTAMP=$(date +%d-%m-%Y_%T)
-MESSAGE="Web service started at: ${TIMESTAMP}"
+MESSAGE="${TIMESTAMP} - Kubernetes probes demo - Web service started"
 echo "${MESSAGE}"
-echo "<h1>Kubernetes probes demo - ${MESSAGE}</h1>" > ${DOCUMENT_ROOT}/index.html
+echo "<h1>${MESSAGE}</h1>" > ${DOCUMENT_ROOT}/index.html
 
 # Run whatever was passed in CMD:
 echo "Exec-uting: $@"
